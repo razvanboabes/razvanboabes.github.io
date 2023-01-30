52419582bc1fcc451c48f63c399ee4b1753b295d
razvanboabes <razvanboabes2005@gmail.com>
2023-01-30 22:48:05 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const handleChange = event => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="App">
+      <img src="./logo.png" alt="Logo" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <textarea
+          placeholder="Message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
